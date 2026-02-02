f5e376407535e0ad78ef8e2444eff0bb32c71bd4
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-02 11:25:54 -0500

    Debugging Priestess Optimal Build

--- Presona Optimal Build Program debug.py
+++ Presona Optimal Build Program debug.py
@@ -1,4 +1,6 @@
 # Test program for debugging and testing for the Optimal Build main program
+# 23 elements in the number of arcanas (22 indexes)
+# 16 elements in the number of skill (15 indexes)
 
 class OptimalBuildSystem:
     list_of_arcanas = ("Fool","Magician","Priestes","Empress","Emperor","Hierophant",
@@ -9,11 +11,17 @@ class OptimalBuildSystem:
     skill_types = ("Phys","Gun","Fire","Ice","Elec","Wind","Psy","Nuke","Bless","Curse",
                 "Buff","Debuff","Passive Buff","HP Recovery","Ailment Recovery","Passives") # A tuple that will hold the list of all skill types 
 
-    # Function that displays the optimal build template for Magician Personas | WIP
-    def display_magician_optimal_build(arcana=f"{list_of_arcanas[1]}", **kwargs,): # using kwargs for the element + number
+    # Function that displays the optimal build template for Priestess Personas | WIP
+    def display_priestess_optimal_build(arcana=f"{list_of_arcanas[2]}"):
         print(f"{arcana} Persona Optimal Build Template:")
-        for element, skill_quantity in kwargs.items():
-            print(f"{element} - {skill_quantity} skills")
+        print("")
 
 
-OptimalBuildSystem.display_magician_optimal_build()  # Debugging 
+# For Magician, I want it the key to be the value of the index in the tuple to avoid having to hard code and being more efficient 
+
+#OptimalBuildSystem.display_magician_optimal_build({skill_types[0]}"=8, f"{skill_types[0]}"=2, f"{skill_types[0]}"=1, f"{skill_types[0]}"=1, f"{skill_types[0]}"=1)
+ # Debugging 
+
+priestess_skills = {}
+
+
