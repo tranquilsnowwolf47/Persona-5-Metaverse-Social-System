2751e03cf5cab7ba947bd020f55618646ece1bb6
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-07 12:28:21 -0500

    Create PersonaStatAllotmentV2.py
    Uses OOP instead of dictionaries

---
+++ PersonaStatAllotmentV2.py
@@ -0,0 +1,16 @@
+class Stats:
+    def __init__(self, allotment_points,
+    st, ma, en, ag, lu):
+        self.allotment_points = allotment points
+        self.st = st
+        self.ma = ma
+        self.en = en
+        self.ag = ag
+        self.lu = lu
+        
+    se
+        
+    def add_st_points(self):
+        input("Please enter the number of st points to add: 
+        self.st += 
+      
