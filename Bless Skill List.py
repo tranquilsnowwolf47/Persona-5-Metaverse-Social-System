5bcd7afad907fbdd25bdba064fa013eef7182d2b
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-10 16:08:19 -0500

    Create Bless Skill List.py

---
+++ Bless Skill List.py
@@ -0,0 +1,18 @@
+# Filename: Bless Skill List.py
+# Date: 2/10/26
+# Author: Aoi | shadowsnowwolf
+# List of Bless skills (Kindness/Warmth)
+
+class BlessSkillls:
+    def __init__(self, name, element, damage_grade, SP_cost, description):
+        self.name = name
+        self.element = element
+        self.damage_grade = damage_grade
+        self.SP_cost = SP_cost 
+        self.description = description 
+
+    # Shows the full info of the skill
+    def display_skill(self):
+        print(f"- {self.name} ({self.element})")
+        print(f"SP Cost: {self.SP_cost}")
+        print(f"Description:\n{self.description}")
