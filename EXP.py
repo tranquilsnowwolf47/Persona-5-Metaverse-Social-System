4801a3570a978262e319fa459c0ba43376198d26
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-11 14:25:54 -0500

    Create EXP.py

---
+++ EXP.py
@@ -0,0 +1,23 @@
+class EXPSystem:
+    pass
+
+
+EXP = 3
+
+RED_SHADOW_ambushed =  
+YELLOW_SHADOW_ambushed = 7 
+GREEN_SHADOW_ambushed =
+
+
+RED_SHADOW_normal =
+YELLOW_SHADOW_normal =
+GREEN_SHADOW_normal =
+
+RED_SHADOW_ambushing =
+YELLOW_SHADOW_ambushing =
+GREEN_SHADOW_ambushing = 
+
+RSA1-
+
+RSA2
+
