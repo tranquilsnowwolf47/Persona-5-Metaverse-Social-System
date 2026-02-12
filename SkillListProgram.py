7f60a01204d77b34245aa640eba71301228e013f
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-12 11:24:54 -0500

    Create SkillListProgram.py

---
+++ SkillListProgram.py
@@ -0,0 +1,9 @@
+# Filename: SkillListProgram.py 
+# Date: 2/12/26
+# Author: Aoi | shadowsnowwolf
+
+# Processing:
+# A program that has a full list of all Persona skills
+# Imports the Skill lists as modules 
+
+import PhysSkillList
