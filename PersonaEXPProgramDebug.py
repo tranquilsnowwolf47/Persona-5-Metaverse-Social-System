61af96f9a1bb8e2d260037dce21197a486e32f2d
Aoi <spadesxoathkeeper17@gmail.com>
2026-03-26 11:25:54 -0400

    Update PersonaEXPProgramDebug.py

--- PersonaEXPProgramDebug.py
+++ PersonaEXPProgramDebug.py
@@ -1,18 +1,32 @@
-import PersonaEXPProgram
+import PersonaEXPProgramV1
 
-kaguya = PersonaEXPProgram.Persona("Kaguya","Moon",35,1,12,8,13,5,0)
-barong = PersonaEXPProgram.Persona("Barong","Emperor","35",15,11,7,5,1,0)
+#kaguya = PersonaEXPProgram.Persona("Kaguya","Moon",35,1,12,8,13,5,0)
+#barong = PersonaEXPProgram.Persona("Barong","Emperor","35",15,11,7,5,1,0)
 #barong.display_persona_info()
 #barong.level_up()
 #barong.display_persona_info
-izanagi = PersonaEXPProgram.Persona("Izanagi","Fool",35,8,8,6,8,9,0)
+#izanagi = PersonaEXPProgram.Persona("Izanagi","Fool",35,8,8,6,8,9,0)
+#mithras = PersonaEXPProgram.Persona("Mithras","Sun",33,12,12,3,3,7,0)
+#unicorn = PersonaEXPProgram.Persona("Unicorn", "Faith",31,5,4,9,8,9,0)
+kushinada = PersonaEXPProgramV1.Persona("Kushinada","Lovers",15,4,6,1,3,1,3)
+kikuri_hime = PersonaEXPProgramV1.Persona("Kikuri-Hime","Priestess",18,1,6,6,4,1,4)
 
-barong.display_persona_info()
+
+
+lachesis = PersonaEXPProgramV1.Persona("Lachesis","Lachesis",24,3,3,3,7,11,1)
+
+
+lachesis.display_persona_info()
+lachesis.level_up()
+lachesis.display_persona_info()
 print()
-barong.allot_st()
-barong.allot_ma()
-barong.allot_en()
-barong.allot_ag()
-barong.allot_lu()
+lachesis.allot_st()
+lachesis.allot_ma()
+lachesis.allot_en()
+lachesis.allot_ag()
+lachesis.allot_lu()
 print()
-barong.display_persona_info()
+lachesis.display_persona_info()
+
+#PersonaEXPProgram.Persona.display_menu(izanagi)
+
