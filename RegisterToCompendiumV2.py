16876176ea1d0eb3029f1a6045d9dfe1b56fc906
Aoi <spadesxoathkeeper17@gmail.com>
2026-04-23 11:15:32 -0400

    Refactor Compendium class and add persona registration

--- RegisterToCompendiumV2.py
+++ RegisterToCompendiumV2.py
@@ -11,38 +11,102 @@
 
 
 class Compendium:
-    # Function that will register a Persona and return it's info
-    def register_persona(self, name, arcana, level, 
+    # Constructor that sets the attributes for a Persona's info
+    def __init__(self, name, arcana, level, 
     st, ma, en, ag, lu,
     skill_one, skill_two,skill_three, skill_four, skill_five, skill_six,
     skill_seven, skill_eight):
-        persona_info =  f"""Persona Registered:  
-        Name: {name}
-        Arcana: {arcana}
-        Level: {level}
-        Stats: \n------------------------         
-        St: {st}
-        Ma: {ma}
-        En: {en}
-        Ag: {ag}
-        Lu: {lu}
+        # Info attributes
+        self.name = name
+        self.arcana = arcana
+        self.level = level
+       
+        # Stat attributes
+        self.st = st 
+        self.ma = ma 
+        self.en = en 
+        self.ag = ag 
+        self.lu = lu
+       
+        # Skill attributes
+        self.skill_one = skill_one 
+        self.skill_two = skill_two
+        self.skill_three = skill_three 
+        self.skill_four = skill_four
+        self.skill_five = skill_five
+        self.skill_six = skill_six
+        self.skill_seven = skill_seven 
+        self.skill_eight = skill_eight
+        
 
-        Skills:
-        1. {skill_one}
-        2. {skill_two}
-        3. {skill_three}
-        4. {skill_four}
-        5. {skill_five}
-        6. {skill_six}
-        7. {skill_seven}
-        8. {skill_eight}
-        \n"""
+    # Gets input from the user on the Persona they want to register
+    def get_persona_info(self):
+        name = input("Please enter the Persona's name to register: ")
+        arcana = input("Enter the Arcana: ")
+        try:
+            level = int(input("Enter the level: "))
+        except ValueError:
+            print("You did not enter a valid level.")
+        try:
+            st = int(input("Enter the St stat: "))
+        except ValueError:
+            print("Integer values only are accepted.")
+        try:
+            ma = int(input("Enter the stat: "))
+        except ValueError:
+            print("Integer values only are accepted.")
+        try:
+            en = int(input("Enter the stat: "))
+        except ValueError:
+            print("Integer values only are accepted.")
+        try:
+            ag = int(input("Enter the stat: "))
+        except ValueError:
+            print("Integer values only are accepted.")
+        try:
+            lu = int(input("Enter Lu stat: "))
+        except ValueError:
+            print("Integer values only are accepted.")
+
+        return name, arcana, level, st, ma, ag, en, lu
+
+
+    # Format's the data from the user input and returns it's info
+    def set_persona_info(self,):
+        persona_info_input = self.get_persona_info()
+        self.name = persona_info_input[0]
+
+
+        persona_info_output =  f"""
+    Persona Registered:  
+    Name: {self.name}
+    Arcana: {self.arcana}
+    Level: {self.level}
+    Stats: \n------------------------         
+    St: {self.st}
+    Ma: {self.ma}
+    En: {self.en}
+    Ag: {self.ag}
+    Lu: {self.lu}
+
+    Skills:
+    1. {self.skill_one}
+    2. {self.skill_two}
+    3. {self.skill_three}
+    4. {self.skill_four}
+    5. {self.skill_five}
+    6. {self.skill_six}
+    7. {self.skill_seven}
+    8. {self.skill_eight}
+    \n"""
         return persona_info
         
     # Writes the data to the file 
-    def write_persona_data():
+    def register_persona():
         pass
         
+
+    
 
 class Persona(Compendium):
     pass
