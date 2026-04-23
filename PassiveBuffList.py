39bf477a906683e0744ba0073707b772846db87d
Aoi <spadesxoathkeeper17@gmail.com>
2026-04-23 09:13:30 -0400

    Updated SP costs for Passive Buffs

--- PassiveBuffList.py
+++ PassiveBuffList.py
@@ -16,25 +16,25 @@ class PassiveBuffSkills:
         print(f"SP Cost: {self.SP_cost}")
         print(f"Description:\n{self.description}")
 
-social_competence = PassiveBuffSkills("Social Competence", "Passive Atk Buff | Auto-Tarukaja effect", 0,
+social_competence = PassiveBuffSkills("Social Competence", "Passive Atk Buff | Auto-Tarukaja effect", 8,
 """"   - Enter every social encounter with a foundation of practiced social skill. 
 This skill comes from social enhancements from going through many successful social interactions to the point where socializing and using elaborate Persona skills becomes second-nature to you. 
 Your tone, delivery, timing, body language, and speech skill has been trained and internalized, allowing your words and presence to strike with more impact. Confidence is no longer a guess, it's built in. 
 Raises your Atk automatically temporarily upon entering a battle.""")
 
-self_control = PassiveBuffSkills("Self-Control", "Passive Def Buff | Auto-Rakukaja effect", 0,
+self_control = PassiveBuffSkills("Self-Control", "Passive Def Buff | Auto-Rakukaja effect", 8,
 """"   - Utilize strong internal regulation and emotional discipline, which, at the start of any interaction, automatically reinforces your social defenses. 
 The skill causes you to resist overreactions, validation-chasing, frame breaks, or attention/approval seeking. 
 Self- Control creates a wall against internal emotional reactions and undesired behaviors and subtly deflects emotional pressure. 
 Raises your Def automatically temporarily upon entering a battle.""")
 
-human_nature_savant = PassiveBuffSkills("Human Nature Savant", "Passive Ag Buff | Auto-Sukukaja effect", 0,
+human_nature_savant = PassiveBuffSkills("Human Nature Savant", "Passive Ag Buff | Auto-Sukukaja effect", 8,
 """"   - Leverage a deep, intuitive understanding of human behavior and psychology. 
 By understanding the expected reactions, behaviors, and patterns behind Shadows, you enter every interaction with heightened precision and adaptability. 
 Your techniques rarely miss the mark and you know when to dodge tension before it forms.
 Raises your Accuracy/Evasion automatically temporarily upon entering a battle.""")
 
-looksmax = PassiveBuffSkills("Looksmax", "Passive Buff | effect", 0,
+looksmax = PassiveBuffSkills("Looksmax", "Passive Buff | effect", 30,
 """"   -  Enter an encounter with an amplified presence via good looks, enhancing your social power capability. 
 The passive aura grants increased presence, resilience, and calibration, framing yourself as high-value before you even speak and granting a good first impression, making people more likely to take you seriously at the start of an interaction. 
 The way that you feel in general is also enhanced which grants your overall abilities more traction. 
