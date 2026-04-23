13b5e1826347b26cf0017d46b2228b776f1c9672
Aoi <spadesxoathkeeper17@gmail.com>
2026-04-23 17:01:23 -0400

    Refactor skill type display and define elemental skills
    Refactor skill type display into a function and add elemental skills.

--- SkillListProgram.py
+++ SkillListProgram.py
@@ -25,16 +25,24 @@
 valid_skill_types = ("Phys","Gun","Fire","Ice","Elec","Wind","Psy","Nuke","Bless","Curse",
                 "Buff","Debuff","Passive Buff","HP Recovery","Ailment Recovery","Passives", "Status Ailment") # A tuple that will hold the list of all skill types 
 
+# Elemental skills that reference indexes from valid skill types 
+elemental_skills = (valid_skill_types[0],valid_skill_types[1],valid_skill_types[2],valid_skill_types[3],
+valid_skill_types[4],valid_skill_types[5])
 
-print("Skill types:")
-print("-----------------------------")
-for skill_type in valid_skill_types:
-    print(skill_type)
-try:
+def display_skill_types():
+    print("Skill types:")
+    print("-----------------------------")
+    for skill_type in valid_skill_types:
+        print(skill_type)
+        
+def get_skill_type():
     skill_type_choice = input("\nPlease enter a skill type: ") 
-except ValueError:
     if skill_type_choice not in valid_skill_types:
         print("You did not enter a valid skill type.")
-
-# if 
-damage_grade_choice = input("Would you look to choose a specific damage grade? (Y-N): ")
+    else:
+        if skill_type_choice 
+    
+    
+    
+    # If they choose an attack skill
+    #damage_grade_choice = input("Would you like to choose a specific damage grade? (Y-N): ")
