ac28be4ed752972787e3b7672ae9abff3a68d0ad
Aoi <spadesxoathkeeper17@gmail.com>
2026-04-23 16:50:09 -0400

    Comment out skill list imports and update skill types

--- SkillListProgram.py
+++ SkillListProgram.py
@@ -7,29 +7,29 @@
 # Imports the Skill lists as modules 
 
 # Imports the modules of all the skill categories 
-import PhysSkillList
-import GunSkillList
-import FireSkillList
-import IceSkillList
-import ElecSkillList
-import WindSkillList
-import PsySkillList
-import NukeSkillList
-import BlessSkillList
-import CurseSkillList
-import BuffSkillList
-import DebuffSkillList
-import PassiveBuffList
+#import PhysSkillList
+#import GunSkillList
+#import FireSkillList
+#import IceSkillList
+#import ElecSkillList
+#import WindSkillList
+#import PsySkillList
+#import NukeSkillList
+#import BlessSkillList
+#import CurseSkillList
+#import BuffSkillList
+#import DebuffSkillList
+#import PassiveBuffList
 
 
-skill_types = ("Phys","Gun","Fire","Ice","Elec","Wind","Psy","Nuke","Bless","Curse",
+valid_skill_types = ("Phys","Gun","Fire","Ice","Elec","Wind","Psy","Nuke","Bless","Curse",
                 "Buff","Debuff","Passive Buff","HP Recovery","Ailment Recovery","Passives", "Status Ailment") # A tuple that will hold the list of all skill types 
 
 
 print("Skill types:")
-for skill_type in skill_types:
-    print(skill_types)
+for skill_type in valid_skill_types:
+    print(skill_type)
 
-skill_type = input("Pleae enter a skill type: ") 
+skill_type = input("\nPleae enter a skill type: ") 
 # if 
 damage_grade_choice = input("Would you look to choose a specific damage grade? (Y-N): ")
