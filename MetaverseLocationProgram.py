bb6605bcd74ddaa8bbb07a3289f9d8d1ca8afc25
Aoi <spadesxoathkeeper17@gmail.com>
2026-05-07 22:37:45 -0400

    Update MetaverseLocationProgram.py

--- MetaverseLocationProgram.py
+++ MetaverseLocationProgram.py
@@ -13,8 +13,8 @@
 class MetaverseLocation:
     # Contructor that defines the attributes of a Metaverse Location
     def __init__(self, name, metaverse_location_type, location, priority, date, distance, rarity,
-                 social_receptiveness, repeatability, exp_yield, loot_potential, risk, reward, resource_cost, 
-                 treasure_demon_potential, growth_alignment, awakening_potential, date_duration, party_compatability):
+                 social_receptiveness, security_level_sensitivity, repeatability, exp_yield, loot_potential, risk, reward, 
+                 resource_cost, treasure_demon_potential, growth_alignment, awakening_potential, date_duration, party_compatability):
         # Name of the Metaverse Location
         self.name = name
         # Attribute for the Metaverse Location type (Palace, Jail, Mementos)
@@ -31,38 +31,74 @@ class MetaverseLocation:
         self.rarity = rarity
         # How receptive the Shadows in the are are to engaing in combat 
         self.social_receptiveness = social_receptiveness
-        # How accessible the location is and how often you can go to and back from it (includes transportation friction, scheduling feasability)
+        # How strongly the environment reacts to your actions in terms of reputation change 
+        self.security_level_sensitivity = security_level_sensitivity
+        # How often you can realistically and consistently attend the location over time (includes transportation friction, scheduling feasability)
         self.repeatability = repeatability
-        #
+        # The expected amount of experience points/Persona growth/character growth expected to be received from interacting with the location
         self.exp_yield = exp_yield
-        #
+        # The potential to obtain tangible external gains from a location (i.e., money, items)
         self.loot_potential = loot_potential
-        #
+        # The probablity and severity of negative outcomes from engaging with the location
         self.risk = risk
-        #
+        # The overall value expected to be gained (XP, relationships, status, opportunity, etc)
         self.reward = reward
         # The amount of anticipated HP, SP, item usage, or strategy that will be required for successful infiltration
         self.resource_cost = resource_cost
-        #
+        # The potential for high value connections (Treasure Demons) to be present and engagable 
         self.treasure_demon_potential = treasure_demon_potential
-        #
+        # How strongly the location supports long-term evolution and growth trajectory, indepedent of immediate reward
         self.growth_alignment = growth_alignment
-        #
+        # The potential for meeting high quality connections that could be capable of becoming candidate Persona users/Phantom Thieves
         self.awakening_potential = awakening_potential
-        #
+        # How long the event will take place for/be available for in the infiltration run 
         self.date_duration = date_duration
-        #
-        self.party_compataiblity = party_compatability
+        # How likely the party will be able to participate/be aligned with the event (This excludes just myself when it comes to solo runs)
+        self.party_compatability = party_compatability
 
 
+    # Categorize the Metaverse Location types into a tuple 
+    metaverse_location_types = ("Palace", "Jail", "Mementos Location")
+    priority_types = ("Urgent", "High", "Medium", "Low", "Void")
+    distance_types = ("Out-of-state", "Very Far", "Far", "Medium", "Close", "Very Close")
+    rarity_types = ("Ultra-Rare", "Rare", "Uncommon", "Common")
+    social_receptiveness_types = ("Very High", priority_types[1], priority_types[2], priority_types[3])
+    security_level_sensitivity_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    repeatability_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    exp_yield_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    loot_potential_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    risk_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    reward_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    resource_cost_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    treasure_demon_potential_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    growth_alignment_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    awakening_potential_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
+    party_compatability_types = (social_receptiveness_types[0], social_receptiveness_types[1], social_receptiveness_types[2], social_receptiveness_types[3])
 
-    # Categorieze the Metaverse Location types into a tuple 
-    metaverse_location_types = ("Palace", "Jail", "Mementos")
-    priorities = ("Urgent", "High", "Medium", "Low", "Void")
-    date_duration = ("Urgent", "Moderate", "Not Urgent")
 
-    def display_info():
-        print("")
+    date_duration_types = (priority_types[0], "Moderate", "Not Urgent")
+    growth_alignment = ("Ultra-High",priority_types[1], priority_types[2], priority_types[3], "Very Low")
+
+    # Displays all necessary details about the Metaverse Location
+    def display_location_summary(self):
+        print("Location Summary:")
+        print("-------------------------------------")
+        print(f"Name: {self.name}")
+        print(f"Type: {self.metaverse_location_types}")
+        print(f"Location: {self.location}")
+        print(f"Priority: {self.priority}")
+        print(f"Social Receptiveness: {self.social_receptiveness}")
+        print(f"Repeatability: {self.repeatability}")
+        print(f"EXP Yield: {self.exp_yield}")
+        print(f"Loot Potential: {self.loot_potential}")
+        print(f"Risk: {self.risk}")
+        print(f"Reward: {self.reward}")
+        print(f"Resource Cost: {self.resource_cost}")
+        print(f"Treasure Demon Potential {self.treasure_demon_potential}")
+        print(f"Growth Alignment: {self.growth_alignment}")
+        print(f"Awakening Potential: {self.growth_alignment}")
+        print(f"Date Duration: {self.date_duration}")
+        print(f"Party Compatability: {self.party_compataiblity}")
 
 
 class Palace(MetaverseLocation):
@@ -74,30 +110,38 @@ class Jail(MetaverseLocation):
 class MementosFloor(MetaverseLocation):
     pass
 
-
+# Palaces
+#-------------------------------------------------------------------------
 # Palace location objects
-fau = Palace()
+fau = Palace("Florida Atlantic University (FAU)", Palace.metaverse_location_types[0], "Boca Raton, FL", Palace.priority_types[3], "N/A", Palace.distance_types[3], 
+             Palace.rarity_types[], Palace.social_receptiveness_types[])
 supercon = Palace()
 otakufest = Palace()
 megacon = Palace()
 
-palaces = ()
+# Tuple that holds all the list of Palace objects
+palaces = (fau, supercon, otakufest, megacon)
 accepted_palaces = []
 voided_palaces = []
 
+# Jails
+#-------------------------------------------------------------------------
 # Jail location objects
 wpb_jail = Jail()
 boca_jail = Jail()
 miami_jail = Jail()
 
-jails = ()
+# Tuple that holds all the list of Jail objects
+jails = (wpb_jail, boca_jail, miami_jail)
 accepted_jails = []
 voided_jails = []
 
+# Mementos
+#-------------------------------------------------------------------------
 # Mementos Location objects
 hero_hype_con = MementosFloor()
 fau_career_fair = MementosFloor()
 
-
-mementos_floors = ()
+# Tuple that holds all teh list of Mementos floor objects
+mementos_floors = (hero_hype_con, fau_career_fair)
 accepted_mementos_floors = []
