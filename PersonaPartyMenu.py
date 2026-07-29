48dd97e4791e7ccd31070c2f3264fc9e3e0256fa
Aoi <spadesxoathkeeper17@gmail.com>
2026-07-29 10:22:03 -0400

    Create PersonaPartyMenu.py

---
+++ PersonaPartyMenu.py
@@ -0,0 +1,16 @@
+class PersonaMenu:
+    def __init__(self, name, arcana, level):
+        self.name = name
+        self.arcana = arcana
+        self.level = level
+
+    current_persona_party = []
+    reserve_personas = []
+
+
+# use a for loop to get the number of Personas the user wants to operate with
+# then ask for info based on the number of iterations the user asked for
+# then place them in the appropriate lists
+# have the user categorize them (select a party to add to: (current persona party, reserve personas, shafted personas))
+
+    
